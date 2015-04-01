e65adec59754701d9d5d43d01c28a141919a3501
Karol Marcjan <karol.marcjan@gmail.com>
2015-04-01 22:42:15 +0200

    Minor formatting changes

--- src/cljs/react_cljs_blogpost/core.cljs
+++ src/cljs/react_cljs_blogpost/core.cljs
@@ -1,11 +1,11 @@
 (ns react-cljs-blogpost.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [goog.events :as events]
-              [goog.history.EventType :as EventType]
-              [cljsjs.react :as react])
-    (:import goog.History))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [reagent.session :as session]
+            [secretary.core :as secretary :include-macros true]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType]
+            [cljsjs.react :as react])
+  (:import goog.History))
 
 (def board-size 16)
 (def alpha-chars (mapv char (range 97 123)))
@@ -28,8 +28,8 @@
 ; side effecting functions used for hiding/marking revealed cards
 (defn hide-nonmatch! [] (mapv #(swap! % assoc :visible false) (@state :cards)) (swap! state assoc :last-symbol ""))
 (defn mark-match! [symbol] (mapv
-                              #(swap! % assoc :matched true)
-                              (filterv #(= (get @% :symbol) symbol) (@state :cards))))
+                             #(swap! % assoc :matched true)
+                             (filterv #(= (get @% :symbol) symbol) (@state :cards))))
 
 (defn reveal-card! [card-state] (swap! card-state assoc :visible true))
 (defn start-game [] (swap! state assoc :cards (generate-cards)))
@@ -37,59 +37,49 @@
 
 (defn card [card-state]
   (letfn [(handle-card-click! [event]
-    ; pair of cards was revealed, now let's go for another pair step
-    (if (= (revealed-cards-count) 2)
-      (hide-nonmatch!)
-    )
+            ; pair of cards was revealed, now let's go for another pair step
+            (if (= (revealed-cards-count) 2)
+              (hide-nonmatch!))
 
-    ;reveal next card in step
-    (reveal-card! card-state)
+            ;reveal next card in step
+            (reveal-card! card-state)
 
-    ;if 2 of cards are revealed, we have to check parity
-    (if (= (revealed-cards-count) 2)
-      (do
-        (if (= (get @state :last-symbol) (get @card-state :symbol))
-          (do
-            (mark-match! (get @card-state :symbol))
-          )
-        )
-      )
-    )
+            ;if 2 of cards are revealed, we have to check parity
+            (if (and (= (revealed-cards-count) 2)
+                     (= (get @state :last-symbol)
+                        (get @card-state :symbol)))
+              (mark-match! (get @card-state :symbol)))
 
-    ;let's remember last symbol to make comparison in subsequent steps
-    (swap! state assoc :last-symbol (@card-state :symbol))
-  )]
+            ;let's remember last symbol to make comparison in subsequent steps
+            (swap! state assoc :last-symbol (@card-state :symbol)))]
 
-  [:div.card
-    {
-      :onClick handle-card-click!
-      :key (.random js/Math)
-      :class (if (@card-state :matched) "card-matched" "card")
-    }
-    [:span.card-value 
-      {
-        :class (if (@card-state :visible) "card-value" "card-value-hidden")
-      } 
-      (@card-state :symbol)
-    ]
-  ]
-  )
- )
+    [:div.card
+     {:onClick handle-card-click!
+      :key     (.random js/Math)
+      :class   (if (@card-state :matched)
+                 "card-matched"
+                 "card")}
+
+     [:span.card-value
+      {:class (if (@card-state :visible)
+                "card-value"
+                "card-value-hidden")}
+      (@card-state :symbol)]]))
 
 (defn home-page []
   [:div.pexeso
-    [:h2 "Pexeso"]
-    [:div.status (if (= (matched-cards-count) board-size)
-        "Game is finished, congratulations !"
-    )]
+   [:h2 "Pexeso"]
+   [:div.status
+    (if (= (matched-cards-count) board-size)
+      "Game is finished, congratulations !")]
 
-    [:br]
+   [:br]
 
-    [:button.button {:onClick start-game} "Restart game"]
+   [:button.button {:onClick start-game} "Restart game"]
 
-    [:div.board
-        (doall (for [card-state (@state :cards)] (card card-state)))
-    ]])
+   [:div.board
+    (doall (for [card-state (@state :cards)]
+             (card card-state)))]])
 
 (defn current-page []
   [:div [(session/get :current-page)]])
@@ -106,10 +96,8 @@
 ;; must be called after routes have been defined
 (defn hook-browser-navigation! []
   (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
+    (events/listen EventType/NAVIGATE
+                   (fn [event] (secretary/dispatch! (.-token event))))
     (.setEnabled true)))
 
 ;; -------------------------
